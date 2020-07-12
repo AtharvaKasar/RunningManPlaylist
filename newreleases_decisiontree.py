b40c49c5d1208430a84d558105ed0c73556cca8c
Atharva Kasar <19kasara@abschools.org>
2020-07-11 21:16:58 -0400

    documenting idea

---
+++ newreleases_decisiontree.py
@@ -0,0 +1,7 @@
+# use spotify authentication to log in and get to/access a playlist
+
+# create a decision tree based on already chosen songs in the running playlist
+
+# browse through new releases and pick songs from them that would fit the decision tree characteristics?
+
+# add those songs to the playlist
